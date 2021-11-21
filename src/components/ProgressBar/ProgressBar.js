22af8cb2761f96378d9d05f470993427cdc379bb
fbjorn <fbjorn@users.noreply.github.com>
2021-11-21 22:23:58 +0300

    Add initial implementation of ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,67 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components';
-
 import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+const SIZES = {
+    small: {
+        '--height': '8px',
+        '--border-radius': '4px',
+    },
+    medium: {
+        '--height': '12px',
+        '--border-radius': '4px',
+    },
+    large: {
+        '--height': '24px',
+        '--border-radius': '8px',
+    },
+}
+
+const ProgressIndicator = styled.div`
+  position: absolute;
+  width: ${p => p.value}%;
+  background-color: ${COLORS.primary};
+  top: 0;
+  left: 0;
+  border-radius: ${p => p.value === 100 ? 'var(--border-radius)' : 'var(--border-radius) 0 0 var(--border-radius)'};
+  height: var(--height);
+`
+
+const ProgressIndicatorLarge = styled(ProgressIndicator)`
+  width: ${p => (p.value* 0.975)}%;
+  top: 4px;
+  left: 4px;
+  border-radius: ${p => p.value === 100 ? '4px' : '4px 0 0 4px'};
+  height: 16px;
+`
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--border-radius);
+  height: var(--height);
+  position: relative;
+  box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+`
+
+const ProgressBar = ({value, size}) => {
+    const styles = SIZES[size];
+    const Indicator = size === "large"? ProgressIndicatorLarge: ProgressIndicator;
+
+    return (
+        <Wrapper style={styles}>
+            <Indicator
+                value={value}
+                style={styles}
+                role={"progressbar"}
+                aria-valuemin={0}
+                aria-valuenow={value}
+                aria-valuemax={100}
+            >
+                &nbsp;
+            </Indicator>
+        </Wrapper>
+    )
 };
 
 export default ProgressBar;
